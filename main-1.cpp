a03d9fc2b5f590b52d368b9f44f6f1a3651fa4b7
Caleb Pasculli <caleb.pasculli@gmail.com>
2024-03-14 13:11:06 +1030

    m

--- main-1.cpp
+++
@@ -1,8 +0,0 @@
-#include "Truckloads.h"
-#include <iostream>
-int main(void){
-    Truckloads t;
-    std::cout << "Trucks needed: " << t.numTrucks(14,3) << std::endl << std::endl;
-    std::cout << "Trucks needed: " << t.numTrucks(15,1) << std::endl;
-    std::cout << "Trucks needed: " << t.numTrucks(1024,5) << std::endl;
-}
--- main-2.cpp
+++
@@ -1,7 +0,0 @@
-#include "Reverser.h"
-#include <iostream>
-int main(void) {
-Reverser r;
-std::cout << r.reverseString("Caleb") << std::endl;
-std::cout << r.reverseDigit(1234) << std::endl;
-};
